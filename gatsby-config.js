62ae8bf047b9e7ee4c68d05bed4aa20820f7cfff
Akseli Nurmio <akseli@nurmio.fi>
2019-02-01 11:38:12 +0200

    Remove Contentful related stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
-const isDev = process.env.NODE_ENV === "development"
-
 module.exports = {
   siteMetadata: {
     title: "Gatsby Default Starter",
@@ -24,14 +18,5 @@ module.exports = {
     },
     "gatsby-plugin-offline",
     "gatsby-transformer-remark",
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: isDev ? "preview.contentful.com" : "cdn.contentful.com",
-        environment: "master",
-      },
-    },
   ],
 }
