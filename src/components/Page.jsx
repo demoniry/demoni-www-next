feba2f7b639f9c6eef2bb47977bcaa341417299e
Akseli Nurmio <akseli@nurmio.fi>
2019-03-09 15:42:11 +0200

    Add styles to Page and add a header

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Header from "./Header"
+import Container from "./Container"
 import PrettyText from "./PrettyText"
+import "./Page.scss"
 
 function Page({ title, html }) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <PrettyText html={html} />
+    <div className="Page">
+      <Header
+        mainNavigationItems={[
+          { title: "Demoni ry", to: "/", replaceWithLogo: true },
+          { title: "Yhdistys", to: "/yhdistys/" },
+          { title: "Opiskelu", to: "/opiskelu/" },
+          { title: "Tapahtumat", to: "/tapahtumat/" },
+          { title: "Liikunta", to: "/liikunta/" },
+          { title: "Kauppa", to: "https://holvi.com/shop/demoniry/" },
+        ]}
+        secondaryNavigationItems={[
+          { title: "Alasivu", to: "/" },
+          { title: "Toinen alasivu", to: "/" },
+        ]}
+      />
+      <main className="Page__content">
+        <Container forText>
+          <h1 className="Page__title">{title}</h1>
+          <PrettyText html={html} />
+        </Container>
+      </main>
     </div>
   )
 }
